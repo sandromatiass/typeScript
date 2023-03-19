5157415fefd65c8aa1255cf770598b1733d049a9
Sandro Matias <devsandromatias@gmail.com>
2023-03-18 23:05:56 -0300

    Aprendendo Tipos

--- TypeScript-ZeroToHero/modulo-01/tiposBasicos/typeNumberBigint/typeNumberBigint.js
+++ TypeScript-ZeroToHero/modulo-01/tiposBasicos/typeNumberBigint/typeNumberBigint.js
@@ -1,4 +1,15 @@
 "use strict";
 // ==> Exemplos: number
 let num1 = 23.0;
+let num2 = 0x78cf;
+let num3 = 0o577;
+let num4 = 0b110001;
+console.log('Number - Ponto Flutuante ==>', num1);
+console.log('Number - Hexadecimal ==>', num2);
+console.log('Number - Octal ==>', num3);
+console.log('Number - Binário ==>', num4);
+console.log('Number - Ponto Flutuante ==>', typeof (num1));
+console.log('Number - Hexadecimal ==>', typeof (num2));
+console.log('Number - Octal ==>', typeof (num3));
+console.log('Number - Binário ==>', typeof (num4));
 // ==> Exeplos: bigint
--- TypeScript-ZeroToHero/modulo-01/tiposBasicos/typeNumberBigint/typeNumberBigint.ts
+++ TypeScript-ZeroToHero/modulo-01/tiposBasicos/typeNumberBigint/typeNumberBigint.ts
@@ -1,5 +1,19 @@
 // ==> Exemplos: number
 
 let num1: number = 23.0;
+let num2: number = 0x78cf;
+let num3: number = 0o577;
+let num4: number = 0b110001;
+
+console.log('Number - Ponto Flutuante ==>', num1);
+console.log('Number - Hexadecimal ==>', num2);
+console.log('Number - Octal ==>', num3);
+console.log('Number - Binário ==>', num4);
+
+console.log('Number - Ponto Flutuante ==>', typeof(num1));
+console.log('Number - Hexadecimal ==>', typeof(num2));
+console.log('Number - Octal ==>', typeof(num3));
+console.log('Number - Binário ==>', typeof(num4));
+
 
 // ==> Exeplos: bigint
